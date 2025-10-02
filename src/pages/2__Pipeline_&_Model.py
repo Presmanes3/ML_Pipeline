6e5f2fddf2c4a1a66e7e73d8ab4d856291a84d2c
jpresman <javier.presmanes.cardama@cern.ch>
2025-10-02 16:09:45 +0200

    fix: Update page titles in Streamlit configuration for Pipeline & Model and Business Recap

--- src/pages/2__Pipeline_&_Model.py
+++ src/pages/2__Pipeline_&_Model.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Problem Analysis", layout="wide")
+st.set_page_config(page_title="Pipeline & Model", layout="wide")
 
 st.title("Pipeline & Model")
--- src/pages/3__Business_Recap.py
+++ src/pages/3__Business_Recap.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Problem Analysis", layout="wide")
+st.set_page_config(page_title="Business Recap", layout="wide")
 
 st.title("Business Recap")
